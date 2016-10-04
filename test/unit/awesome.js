caefb2d2853f996e9e4e0913ee7122649dd46d46
Ryan Schroeder <ryan.schroeder@veritas.com>
2016-10-03 23:46:41 -0400

    failing on purpose to test stuff

--- test/unit/awesome.js
+++ test/unit/awesome.js
@@ -1,7 +1,13 @@
 'use strict'
 
-let awesome = require('./awesome')
+let awesome = require('../../awesome')
+let expect = require('chai').expect
 
 describe('awesome', () => {
-  it('should return a good message', () => {})
+  it('should return a good message', () => {
+    expect(awesome.eyebrows()).to.equal('THOSE EYEBROWS')
+  })
+  it('should fail here', () => {
+    expect(0).to.equal(1)
+  })
 })
